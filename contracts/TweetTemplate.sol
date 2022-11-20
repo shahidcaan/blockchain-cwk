dfe9eab1046136bd2fc69f6572b4ad578474f436
Shahid Khan <shahidcaan@gmail.com>
2022-11-20 21:39:59 +0100

    Add files via upload

---
+++ contracts/TweetTemplate.sol
@@ -0,0 +1,9 @@
+ // SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.9;
+ struct Tweet {
+        uint id;
+        address username;
+        string tweetContent;
+        bool isDeleted;
+        
+    }
--- hardhat.config.js
+++ hardhat.config.js
@@ -1,18 +1,12 @@
-
-
+require("@nomicfoundation/hardhat-toolbox");
 require('dotenv').config();
-require("@nomiclabs/hardhat-ethers");
-
-const { API_URL, PRIVATE_KEY } = process.env;
-
+/** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
-   solidity: "0.8.17",
-   defaultNetwork: "goerli",
-   networks: {
-      hardhat: {},
-      goerli: {
-         url: API_URL,
-         accounts: [`0x${PRIVATE_KEY}`]
-      }
-   },
-}
+  solidity: "0.8.17",
+  networks: {
+    goerli: {
+      url: process.env.API_URL,
+      accounts: [process.env.PRIVATE_KEY],
+    }
+  }
+};
